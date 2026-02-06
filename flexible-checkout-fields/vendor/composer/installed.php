f46a4db201b6909afbe3b257751024d456b98b52
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 03:36:30 +0000

    4.1.34

--- flexible-checkout-fields/vendor/composer/installed.php
+++ flexible-checkout-fields/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'wpdesk/flexible-checkout-fields',
-        'pretty_version' => '4.1.27',
-        'version' => '4.1.27.0',
-        'reference' => '9150e0975ce5218304c42a6b3a94c61de9a5c750',
+        'pretty_version' => '4.1.34',
+        'version' => '4.1.34.0',
+        'reference' => '4205b36d37ff0b50dfe0425c9ade0bd2108a8f47',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -38,9 +38,9 @@
             'dev_requirement' => false,
         ),
         'wpdesk/flexible-checkout-fields' => array(
-            'pretty_version' => '4.1.27',
-            'version' => '4.1.27.0',
-            'reference' => '9150e0975ce5218304c42a6b3a94c61de9a5c750',
+            'pretty_version' => '4.1.34',
+            'version' => '4.1.34.0',
+            'reference' => '4205b36d37ff0b50dfe0425c9ade0bd2108a8f47',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
